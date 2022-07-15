75bd8ebcf1d4e37178a1b3da11fb1e52e43e010a
Sergey <manofthetreasure@gmail.com>
2022-07-15 18:37:46 +0300

    stickynavBar

---
+++ script.js
@@ -0,0 +1,11 @@
+let nav = document.querySelector("nav");
+
+window.addEventListener("scroll", ()=>{
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky");
+    }
+    else{
+        nav.classList.remove("sticky");
+    }
+
+})
